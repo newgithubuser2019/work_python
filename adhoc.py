8ea225f7b22efcf4ead50b8fb1b33f695520339a
Dmitry Potapov <potapov_d@list.ru>
2026-06-10 17:23:51 +0300

    connection to sql server

--- adhoc.py
+++ adhoc.py
@@ -13,6 +13,9 @@ import numpy as np
 import openpyxl
 import pandas as pd
 import plotly.express as px
+import connectorx as cx
+import polars as pl
+import getpass
 
 import df2tables
 from pandas.tseries.offsets import DateOffset
@@ -36,8 +39,7 @@ USERPROFILE = os.environ["USERPROFILE"]
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # empty dataframes
-# findf = pd.DataFrame()
-df = pd.DataFrame()
+# df = pd.DataFrame()
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # default lists
@@ -61,8 +63,17 @@ df = pd.DataFrame()
 # filename0 = USERPROFILE + "\\Documents\\Работа\\отчетность\\ежедневно\\накопительный отчет\\_промежуточный файл df_впк.xlsx"
 filename1 = "P:\\Documents\\ДБ\\СРП\\Компенсации и льготы\\Премирование\\!СМОТы, встречи, материалы\\!Аналитика\\своды по премиям\\ремаркетинг свод.xlsx"
 
+# sql_username = getpass.getuser()
+# sql_password = getpass.getpass(prompt="SQL password: ", stream=None)
+# sql_password = getpass.getpass(prompt="SQL password: ", stream=None, echo_char="*") # echo_char parameter added in python 3.14
+# uri1 = "mssql://" + sql_username + ":" + sql_password + "@vls-sql-zup-dev:1433/HR_CAB"
+# print(uri1)
+# #sys.exit()
+
+uri1 = "mssql://vls-sql-zup-dev:1433/HR_CAB?trusted_connection=true"
 
 # ------------------------------------------------------------------------------------------------------------------------------------------
+"""
 df_from_excel = pd.read_excel(
     filename1,
     sheet_name="продажи",
@@ -73,6 +84,23 @@ df_from_excel = pd.read_excel(
     # usecols = "A,C,G,H,I,K",
     )
 # print(df_from_excel.info())
-df2tables.render(df_from_excel)
+"""
+#df2tables.render(df_from_excel)
+
+# conn = ""
+# query = "SELECT * FROM dbo.Termination_URPA"
+# df_arrow = cx.read_sql(conn, query, protocol='text', return_type='arrow')
+# df = df_arrow.to_pandas()
+
+# sys.exit()
+
+query = "SELECT * FROM dbo.Termination_URPA"
+df_from_sql = pl.read_database_uri(
+    uri=uri1,
+    query=query,
+    engine="connectorx"
+    )
+print(df_from_sql.head())
+# df2tables.render(df_from_sql)
 
 sys.exit()
